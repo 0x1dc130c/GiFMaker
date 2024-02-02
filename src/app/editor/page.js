7d4a11b6fd3d11edf6f38d8d720e62a2054f0ea3
0x1dc130c <111876371+0x1dc130c@users.noreply.github.com>
2024-02-02 13:29:59 +0700

    create page editor

---
+++ src/app/editor/page.js
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function Editor() {
+  return ( 
+    <div>
+      <Navbar/>
+      <div class="grid place-items-center ">
+          <div class="w-72 h-54 bg-blue-500 m-4 "></div>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
+      width: {
+        '128': '32rem',
+        '72': '52.5rem',
+      },
+      height: {
+        '54': '37.5rem',
+        '128': '32rem',
+      },
+      size: {
+        '48': "12rem",
+        '70': "17.5rem",
+        '80': "42rem"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -14,5 +28,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 };
