21af933f6475140101ac17c110167ef92d7ac9fd
0x1dc130c <111876371+0x1dc130c@users.noreply.github.com>
2024-02-02 14:46:19 +0700

    remove dev test123

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,6 @@ function Navbar() {
     <div>
         test123
     </div>
-
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/>
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
     </a>
